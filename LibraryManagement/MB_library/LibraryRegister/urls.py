58c50c856836dbfcded6e8ee2edd5043f2b39cb2
maskur15 <sabilhasan2018@gmail.com>
2023-02-25 16:30:45 +0600

    Library Register updated edit delete action added in views also the form added

--- LibraryManagement/MB_library/LibraryRegister/urls.py
+++ LibraryManagement/MB_library/LibraryRegister/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views 
 urlpatterns=[
     path('home/',views.showHome),
-    path('',views.showMain),
+    path('',views.showMain,name='home'),
+    path('search/<str:bk>/',views.search,name='search'),
+    path('create-book/',views.createBook,name='create-book'),
+    path('update-book/<str:bookKey>/',views.updateBook,name='update-book'),
+    path('delete-book/<str:bookKey>/', views.deleteBook, name='delete-book'),
 
-    path('search/<str:bk>/',views.search,name='search')
 ]
--- LibraryManagement/MB_library/LibraryRegister/views.py
+++ LibraryManagement/MB_library/LibraryRegister/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 # Create your views here.
 from .models import Book
+from .forms import BookForm
 bookList =[
     {'id':1,'author':'wakedi' , 'title':'History of nobel man','rated':True},
     {"id":2 , 'author': 'kathy sierra', 'title': 'java programming','rated':False},
@@ -19,5 +20,31 @@ def search(request,bk):
     bookObject= Book.objects.get(id=bk)
     context={"book":bookObject,'bookKey':bk}
     return render(request,'LibraryRegister/details.html',context)
+def createBook(request):
+    form = BookForm()
+    if request.method=='POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        print('form data ',request.POST)
+    context={'form':form}
+    return render(request,'LibraryRegister/book-form.html',context)
+def updateBook(request,bookKey):
+    book = Book.objects.get(id=bookKey)
+    form = BookForm(instance=book)
 
+    if request.method=='POST':
+        form = BookForm(request.POST,instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
+    context={'form':form}
+    return render(request,'LibraryRegister/book-form.html',context)
+def deleteBook(request,bookKey):
+    book= Book.objects.get(id=bookKey)
+    if request.method=='POST':
+        book.delete()
+        return  redirect('home')
+    return render(request,'LibraryRegister/book-delete.html',{'book':book})
