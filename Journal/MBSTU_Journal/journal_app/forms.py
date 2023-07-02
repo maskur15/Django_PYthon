545cd9be7a24547282bf5e32ad506a69422af471
maskur15 <sabilhasan2018@gmail.com>
2023-07-02 12:13:20 +0600

    project Management app started

--- Journal/MBSTU_Journal/journal_app/forms.py
+++ Journal/MBSTU_Journal/journal_app/forms.py
@@ -4,4 +4,9 @@ class paperForm(ModelForm):
     class Meta: 
         model = Papers 
         fields = '__all__'
-
+    
+    def __init__(self,*args,**kwargs):
+        super(paperForm,self).__init__(*args,**kwargs)
+        for key,value in self.fields.items():
+            value.widget.attrs.update({'class': 'input'})
+       # self.fields['title'].widget.attrs.update({'class':'input'})
