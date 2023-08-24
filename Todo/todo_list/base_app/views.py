12c8ffd23576f637ef710122f4ff5c4e9a99f151
maskur15 <sabilhasan2018@gmail.com>
2023-08-24 17:30:10 +0600

    task list html file created where we can get the views from the taskdetail. set the context_object name in views where we send the class view

--- Todo/todo_list/base_app/views.py
+++ Todo/todo_list/base_app/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 from django.views.generic.list import ListView
 from .models import Task
+from django.views.generic.detail import DetailView
 
 class TaskList(ListView):
     model = Task
-    
+    context_object_name= 'tasks'
+
+class TaskDetail(DetailView):
+    #detail view return the specific item 
+    model = Task 
+    context_object_name='task'
+    template_name='base_app/task.html'
