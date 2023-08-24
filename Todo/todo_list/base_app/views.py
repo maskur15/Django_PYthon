5d49d48b4a880f3cffd67c6bac74c97e31f511f6
maskur15 <sabilhasan2018@gmail.com>
2023-08-24 18:19:20 +0600

    create delete and update are implemented using class view

--- Todo/todo_list/base_app/views.py
+++ Todo/todo_list/base_app/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from django.views.generic.list import ListView
-from .models import Task
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView ,UpdateView,DeleteView
+from django.urls import reverse_lazy 
+from .models import Task
+
 
 class TaskList(ListView):
     model = Task
@@ -15,3 +18,18 @@ class TaskDetail(DetailView):
     model = Task 
     context_object_name='task'
     template_name='base_app/task.html'
+
+class TaskCreate(CreateView):
+    model= Task 
+    fields= '__all__'
+    success_url= reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields= '__all__'
+    success_url=reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name= 'task'
+    success_url= reverse_lazy('tasks')
