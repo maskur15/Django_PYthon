e97d19f363eccfcedc38144dc73e9ecef37fb25c
maskur15 <sabilhasan2018@gmail.com>
2023-08-27 07:50:05 +0600

    LoginRequiredmixin added in views so that user must be loggedin . also in  settings LOGIN_URL ADDED

--- Todo/todo_list/base_app/views.py
+++ Todo/todo_list/base_app/views.py
@@ -7,6 +7,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView ,UpdateView,DeleteView
 from django.urls import reverse_lazy 
 from django.contrib.auth.views import LoginView 
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 from .models import Task
 
@@ -14,35 +15,33 @@ class CustomLogin(LoginView):
     template_name= 'base_app/login.html'
     fields = '__all__'
     redirect_authenticated_user = True 
-    print('maskur al shal sabil')
-
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     #this view will return all the tasks in the database 
     model = Task
     context_object_name= 'tasks'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin,DetailView):
     #detail view return the specific item 
     model = Task 
     context_object_name='task'
     template_name='base_app/task.html' # this html file will be looked when this view called 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     #this view will look for a html template task_form 
     model= Task 
     fields= '__all__'
     success_url= reverse_lazy('tasks') # after submitting the form 'tasks' url will be called
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     #this view will also look for a html template task_form
     model = Task
     fields= '__all__'
     success_url=reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     #this view will look for task_confirm delete html page 
     model = Task
     context_object_name= 'task'
