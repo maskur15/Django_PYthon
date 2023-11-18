9436465e03d143e6dc27991f8b37da90af9f024c
maskur15 <sabilhasan2018@gmail.com>
2023-11-18 08:20:12 +0600

    front and models

--- Ecommerce/ecommerce/store/models.py
+++ Ecommerce/ecommerce/store/models.py
@@ -13,9 +13,17 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=False)
+    images = models.ImageField(null=True,blank=True)
 
     def __str__(self) -> str:
         return self.name
+    @property
+    def imgUrl(self):
+        try:
+            url = self.images.url
+        except Exception:
+            url=""
+        return url 
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
     date_orderd =models.DateTimeField(auto_now_add=True)
